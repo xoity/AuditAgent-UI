a4acb12662cc17dbd44906ca7ace4eabf64c5a1e
Mohammad Abu-Khader <mohammad.abukhader@hotmail.com>
2025-11-28 12:20:58 +0400

    byebye
    Signed-off-by: Mohammad Abu-Khader <mohammad.abukhader@hotmail.com>

--- web/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import ensure_csrf_cookie
-
-
-@ensure_csrf_cookie
-def index(request):
-    """Serve the React frontend."""
-    return render(request, "web/index.html")
-
